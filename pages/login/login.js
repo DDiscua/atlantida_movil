54227f199b15b0bfc970b173d7d41557e8cbc4e2
DDiscua <31171755+DDiscua@users.noreply.github.com>
2018-01-26 09:14:21 -0600

    init configuration

---
+++ pages/login/login.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+class Login extends Component {
+  constructor(props) {
+    super();
+    this.state = {};
+  }
+
+  componentWillMount() {}
+  componentDidMount() {}
+
+  render() {
+    return (
+      <div className="primary_container">
+        <h1>LOGIN</h1>
+      </div>
+    );
+  }
+}
+
+export default Login;
---
+++ routing/routes.js
@@ -0,0 +1,8 @@
+import { Login } from "../pages/login/login";
+
+const routes = [
+  {
+    path: "/",
+    component: Login
+  }
+];
